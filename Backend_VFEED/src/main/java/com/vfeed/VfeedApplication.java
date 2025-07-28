b485f213f19b659ca34da13081518d9808232ac9
koulapuresumit <koulapuresumit@gmail.com>
2025-07-28 15:20:04 +0530

    Backend template for vfeed

---
+++ Backend_VFEED/src/main/java/com/vfeed/VfeedApplication.java
@@ -0,0 +1,13 @@
+package com.vfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VfeedApplication.class, args);
+	}
+
+}
