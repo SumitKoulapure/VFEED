91083e8ab729f3a889be8c483fa41365d547ab0e
Shrishail Khot <khotshrishail75@gmail.com>
2025-08-04 00:29:49 +0530

    Category Entity

--- Backend_VFEED/src/main/java/com/vfeed/model/Category.java
+++ Backend_VFEED/src/main/java/com/vfeed/model/Category.java
@@ -1,5 +1,33 @@
 package com.vfeed.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
+
+    private  String name;
+
+    @ManyToOne
+    @JsonIgnore
+    private Restaurant restaurant;
+
+
 
 }
