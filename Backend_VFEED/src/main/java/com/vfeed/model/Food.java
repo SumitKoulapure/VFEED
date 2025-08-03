b08ce8096b373b9152c28f86c51455e56ef5b540
komal573 <komalwable4@gmail.com>
2025-08-03 19:15:51 +0530

    Restro-Food-Entity

---
+++ Backend_VFEED/src/main/java/com/vfeed/model/Food.java
@@ -0,0 +1,53 @@
+package com.vfeed.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+public class Food {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	
+	private String description;
+	
+	private Long price;
+	
+	
+	@ManyToOne
+	private Category foodCategory;
+	
+	@Column(length = 1000)
+	@ElementCollection
+	private List<String> images;
+	
+	private boolean available;
+	
+	@ManyToOne
+	private Restaurant restaurant;
+	
+	private boolean isVegeterian;
+	private boolean isSeasonable;
+	
+	@ManyToMany
+	private List<IngredientsItem> ingredients=new ArrayList<>();
+	
+	 
+	private Date creationDate;
+	
+	
+	
+	
+
+}
