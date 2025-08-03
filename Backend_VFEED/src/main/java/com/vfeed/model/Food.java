ae5141b5ecf89cefc233ad9922c10d267210450f
KD3_89298_Sanket <sanketkhokadesk@gmail.com>
2025-08-03 23:50:29 +0530

    Added User,Order,OrderItem and RestaurantDto

--- Backend_VFEED/src/main/java/com/vfeed/model/Food.java
+++ Backend_VFEED/src/main/java/com/vfeed/model/Food.java
@@ -4,14 +4,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
+@Entity
 public class Food {
 	
 	@Id
---
+++ Backend_VFEED/src/main/java/com/vfeed/model/Order.java
@@ -0,0 +1,47 @@
+package com.vfeed.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@ManyToOne //Many order have same user
+	private User customer;
+
+	@JsonIgnore
+	// Whenever we fetch the order object that time we dont need restaurant object in order so we used here @JsonIgnore
+	@ManyToOne //One restaurant have many order i.e Many order have same restaurant
+	private Restaurant restaurant; // From which restaurant customer made this order
+
+	private Long totalAmount;
+
+	private String orderStatus;
+
+	private Date createdAt;
+
+	@ManyToOne //Many orders have same address
+	private Address deliveryAddress;
+
+	@OneToMany // one order have many items
+	private List<OrderItem> items;
+
+//    private Payment payment;
+
+	private int totalItem;
+
+	private int totalPrice;
+	
+}
