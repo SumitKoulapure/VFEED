2902b448d271a47748550f162b8cb1efe21586b7
KD3_89298_Sanket <sanketkhokadesk@gmail.com>
2025-08-05 01:46:21 +0530

    Adding AddCartItemRequest

--- Backend_VFEED/src/main/java/com/vfeed/service/CartService.java
+++ Backend_VFEED/src/main/java/com/vfeed/service/CartService.java
@@ -1,7 +1,15 @@
 package com.vfeed.service;
 
+import com.vfeed.model.Cart;
 import com.vfeed.model.CartItem;
+import com.vfeed.request.AddCartItemRequest;
 
 public interface CartService {
-    public CartItem addItemToCart(AddCartItemRequest req,String jwt) throws Exception;
+    public CartItem addItemToCart(AddCartItemRequest req, String jwt) throws Exception;
+
+    public CartItem updateCartItem(Long cartItemid,int quantity) throws Exception;
+
+    public Cart removeItemFromCart(Long cartItemId,String jwt) throws Exception;
+
+    
 }
