610e90925aa5e883e75986312dd39a74ee11ba5f
koulapuresumit <koulapuresumit@gmail.com>
2025-08-04 15:00:57 +0530

    User Entity Added

--- Backend_VFEED/src/main/java/com/vfeed/model/User.java
+++ Backend_VFEED/src/main/java/com/vfeed/model/User.java
@@ -1,46 +1,44 @@
 package com.vfeed.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.vfeed.dto.RestaurantDto;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.vfeed.dto.RestaurantDto;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
 
 @Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
 
-    private String fullName;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
-    private String email;
+	private String fullName;
+	private String email;
+	private String password;
 
-    private String password;
+	private USER_ROLE role=USER_ROLE.ROLE_CUSTOMER;
 
-    private USER_ROLE role;
+	@JsonIgnore
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+	private List<Order> orders;
 
-
-    @JsonIgnore
-    @OneToMany(cascade = CascadeType.ALL,mappedBy = "customer")
-    private List<Order> orders = new ArrayList<>();
-
-    @ElementCollection
-    private List<RestaurantDto> favorites = new ArrayList<>();
-
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<Address> addresses = new ArrayList<>();
-
-
-
-
+	@ElementCollection
+	private List<RestaurantDto> favorites=new ArrayList<>();
+	
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Address> addresses = new ArrayList<>();
+	
+	private String status;
 
 }
