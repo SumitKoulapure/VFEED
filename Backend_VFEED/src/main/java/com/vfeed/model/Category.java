785f36606c1fefe17728dc3141e4b494ac4a70b2
koulapuresumit <koulapuresumit@gmail.com>
2025-08-04 15:12:35 +0530

    minner changes

--- Backend_VFEED/src/main/java/com/vfeed/model/Category.java
+++ Backend_VFEED/src/main/java/com/vfeed/model/Category.java
@@ -1,14 +1,15 @@
 package com.vfeed.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Data
