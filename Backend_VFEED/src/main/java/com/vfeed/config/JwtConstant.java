907322a69b0ed94feea6ce2e1900058e88139dad
koulapuresumit <koulapuresumit@gmail.com>
2025-08-04 16:03:18 +0530

    Jwt consatant Secret key

---
+++ Backend_VFEED/src/main/java/com/vfeed/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.vfeed.config;
+
+public class JwtConstant {
+
+
+	public static final String SECRET_KEY="wpembytrwcvnryxhjkksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
+	public static final String JWT_HEADER="Authorization";
+	
+}
