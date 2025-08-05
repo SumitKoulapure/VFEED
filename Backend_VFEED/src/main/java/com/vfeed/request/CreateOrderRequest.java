3badecb8364f78a4f28f637939df39dc54e70936
KD3_89298_Sanket <sanketkhokadesk@gmail.com>
2025-08-05 15:54:34 +0530

    Adding CreateOrderRequest

---
+++ Backend_VFEED/src/main/java/com/vfeed/request/CreateOrderRequest.java
@@ -0,0 +1,14 @@
+package com.vfeed.request;
+
+import com.vfeed.model.Address;
+
+import lombok.Data;
+
+@Data
+public class CreateOrderRequest {
+
+    private Long restaurantId;
+
+    private Address deliveryAddress;
+    
+}
